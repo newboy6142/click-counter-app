bcdcd794be3ede92882c12330330948a8f6a80df
newboy6142 <newboy6142@gmail.com>
2025-06-02 11:56:14 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+let totalClicks = 0;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.emit('updateTotal', totalClicks);
+
+  socket.on('click', () => {
+    totalClicks++;
+    io.emit('updateTotal', totalClicks);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(\`Server running on port \${PORT}\`);
+});
